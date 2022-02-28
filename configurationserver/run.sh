8e2d8a87dab58d7e5469b24c1ced3d685a8cc76a
jgonzalezh78 <jgonzalezh78@gmail.com>
2022-02-28 17:33:26 -0600

    microservicios Hoxton.SR3
    Cambio en la configuración para levantar en localhost

---
+++ configurationserver/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting License Server"
+echo "********************************************************"
+java -jar /usr/local/configuracionservice/configurationserver-0.0.1-SNAPSHOT.jar
---
+++ zuulserver/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting License Server"
+echo "********************************************************"
+java -jar /usr/local/zuulservice/zuulserver-0.0.1-SNAPSHOT.jar
